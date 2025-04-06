752a6c467d42cedf15d98d922da84a505f38e6ca
Antonio M <kirye2018@gmail.com>
2025-04-05 19:18:33 -0600

    Dialogo

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -6,7 +6,7 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(0);
+        SceneManager.LoadSceneAsync(2);
     }
 
     public void QuitGame()
